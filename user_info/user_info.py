67d8eb3f77143a4b441483396b4bea5091c0ba2a
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-20 20:06:58 +0530

    Create user_info.py

---
+++ user_info/user_info.py
@@ -0,0 +1,23 @@
+from json import load
+
+
+class User_Info():
+    def __init__(self):
+        # json with your token
+        user_info_directory = "user_info/.user_info.json" 
+        self.user_info = self.__load_user_info(user_info_directory)
+
+
+        self.token = self.user_info["token"]
+        self.forwarding_list_of_all_users = self.user_info[
+            "forwarding_list_of_all_users"]
+
+
+    def __load_user_info (self, json_directory):
+        with open(json_directory, "r") as user_info_json:
+            return load(user_info_json)
+
+
+user_info = User_Info()
+token = user_info.token
+forwarding_list_of_all_users = user_info.forwarding_list_of_all_users
