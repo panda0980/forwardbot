69b70f7dbb9e39abebda18c442d13b7d224acaf7
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-23 01:56:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
 import os
-
-class Config(object):
+import logging
+class Config:
     
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
+    OWNER_ID = os.environ.get("OWNER_ID", "")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
+    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
+    SESSION = os.environ.get("SESSION", "Forward_Session")
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", None)
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-    API_ID = int(os.environ.get("API_ID"))
-
-    API_HASH = os.environ.get("API_HASH")    
-    
-    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
-
-    CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
-
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
