5df42d6321867d98a339eeec888467d7d4d8a536
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-23 01:53:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,45 @@
-from user_info.user_info import token
-from telegram.ext import Updater
-from handlers import (
-    start_command,
-    help_command,
-    show_list_command,
-    add_title_to_waiting_list_command, 
-    remove_group_to_forward_list_command,
-    bot_added_to_group_handler,
-    forward_handler)
+from pyrogram import Client, __version__
+
+from config import Config
+from config import LOGGER
+
+from user import User
+import pyromod.listen
 
 
-def main():
-    updater = Updater(token, use_context=True)
-    dp = updater.dispatcher
+class Bot(Client):
+    USER: User = None
+    USER_ID: int = None
 
+    def __init__(self):
+        super().__init__(
+            Config.BOT_SESSION,
+            api_hash=Config.API_HASH,
+            api_id=Config.API_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=10,
+            bot_token=Config.BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
 
-    dp.add_handler(start_command)
-    dp.add_handler(help_command)
-    dp.add_handler(show_list_command)
-    dp.add_handler(add_title_to_waiting_list_command)
-    dp.add_handler(remove_group_to_forward_list_command)
-    dp.add_handler(bot_added_to_group_handler)
-    dp.add_handler(forward_handler)
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.set_parse_mode("html")
+        self.LOGGER(__name__).info(
+            f"@{usr_bot_me.username}  started! "
+        )
+        self.USER, self.USER_ID = await User().start()
 
-
-    print("\nbot started")
-    updater.start_polling()
-    updater.idle()
-    print("\nbot endded")
-
-
-if __name__ == '__main__':
-    main()
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
+        
+        
+app = Bot()
+app.run
+        
+        
+        
