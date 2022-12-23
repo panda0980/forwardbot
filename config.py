bd83b1ad76f2e7503c84e69d2331ec9dfb13d17b
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-23 19:07:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import os
 import logging
+
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
-    OWNER_ID = os.environ.get("OWNER_ID", "")
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", None)
-
-
+    BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
+    CAPTION = os.environ.get("CAPTION", "")
+    FROM_CHANNEL = os.environ.get("FROM_CHANNEL", None)
+    FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
+    OWNER_ID = os.environ.get("OWNER_ID", 12345)
+    LIMIT = int(os.environ.get("LIMIT", "25000"))
+    SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
+    SESSION = os.environ.get("SESSION")
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", 12345))
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
