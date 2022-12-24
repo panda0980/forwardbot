b662c419d60b74a11442ebd68e63f197eac1944c
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-24 12:33:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     OWNER_ID = environ.get("OWNER_ID", "1983530070")
     LIMIT = int(environ.get("LIMIT", "25000"))
     SKIP_NO = int(environ.get("SKIP_NO", "0"))
-    SESSION = environ.getenv("SESSION", "BQAo3w6AHb9jOnnCRyiDBRIyFSX7PWisgQKSttm3q2HetCeqMI6kViupUKycgUwuBfMr8-GawlSuyRLREouTPYlj8C6VnDGwaHqkT3c5bSHXig8ua3mKFayhECY8UjxB52QcbZtdVaZ-GEw_Tsd7g7Ddqbygu9tep65Qgvj6GXOnnAJPT80O0sHUDqPdLv2_P3yQDbTwwmvqzXvKGKiMoFLW83l9rosZTELNAtfrmBZ5vXjp797jJ-m7gMhZBGhVlImRX9n5hld7OsRfHdTA-5u6u9T8bjS2dr_vwB1fpHIqmv6A2W__veGWoUv5GUxODmcwDGUxzBqpXGsAYoOgi1CEdjpEVgA")
+    SESSION = environ.get("SESSION", "BQAo3w6AHb9jOnnCRyiDBRIyFSX7PWisgQKSttm3q2HetCeqMI6kViupUKycgUwuBfMr8-GawlSuyRLREouTPYlj8C6VnDGwaHqkT3c5bSHXig8ua3mKFayhECY8UjxB52QcbZtdVaZ-GEw_Tsd7g7Ddqbygu9tep65Qgvj6GXOnnAJPT80O0sHUDqPdLv2_P3yQDbTwwmvqzXvKGKiMoFLW83l9rosZTELNAtfrmBZ5vXjp797jJ-m7gMhZBGhVlImRX9n5hld7OsRfHdTA-5u6u9T8bjS2dr_vwB1fpHIqmv6A2W__veGWoUv5GUxODmcwDGUxzBqpXGsAYoOgi1CEdjpEVgA")
     TO_CHANNEL = int(environ.get("TO_CHANNEL", "-1001629175532"))
 
 def LOGGER(name: str) -> logging.Logger:
