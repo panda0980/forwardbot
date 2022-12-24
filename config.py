8ace69d80ccf22bc6f54744bd27c2b7fd0c46770
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-24 17:42:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import logging
 
 class Config:
     
-    API_ID = int(environ.get("API_ID", "11671320"))
-    API_HASH = environ.get("API_HASH", "8e409e260f1d80f0ead65da912ee07bb")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "5813212548:AAG1503Ilf2se-uiHkWwNSxPSr20v0yL6Oo") 
+    API_ID = int(environ.get("API_ID", ""))
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     CAPTION = environ.get("CAPTION", "")
     FROM_CHANNEL = environ.get("FROM_CHANNEL", None)
     FILTER_TYPE = environ.get("FILTER_TYPE", "")
-    OWNER_ID = environ.get("OWNER_ID", "1983530070")
+    OWNER_ID = environ.get("OWNER_ID", "")
     LIMIT = int(environ.get("LIMIT", "25000"))
     SKIP_NO = int(environ.get("SKIP_NO", "0"))
-    SESSION = environ.get("SESSION", "BQBtDYfn0sUsI76Bhk6Ez-9Jx-Px-_TP50c4ZdXifWRdwVRcCXSvGSdXQ53X4aqdq0Wp5ptiSA-ql0waGMHDCQ_-NZmGvU4HkAGaMJvbg8FTgB6d9J3iBxFyeWmZ8t8CRe2jSyF7vvQ568FEGJ8cnoa8yreNhVJNxK_C8g3n1MHpt2jX0iwoPnwYByCl6o7zKIUyW3CFd0LP-o3h5tFrq3IDBgpnpUiVyfNdXSJ-q0jdeegbrEq-6ApotTWxS580NpbW1iq3fw21sBYrDvcFFxmhcx3uK5EIkvEJ4MLNoClQt9JSYtmgHNiKDBpIvSgwbIFX9dgBIBtSSbBxNb_e64X5djpEVgA")
-    TO_CHANNEL = int(environ.get("TO_CHANNEL", "-1001629175532"))
+    SESSION = environ.get("SESSION", "")
+    TO_CHANNEL = int(environ.get("TO_CHANNEL", ""))
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
